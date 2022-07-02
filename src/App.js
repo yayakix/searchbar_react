1b1755a138ee423e51c5cff34e3e59283fcb9cbb
Iyana Marquez <yayakix@gmail.com>
2022-07-02 18:16:02 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import data from './data';
+import { useEffect, useState } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
-  const [monster, setMonster] = useState([])
+  const [monster, setMonster] = useState([]);
   useEffect(() => {
     setMonster(data);
-    
-  }, []) 
-  const [currsearch, setCurrSearch] =  useState('')
+  }, []);
+  const [currsearch, setCurrSearch] = useState("");
 
-// function handleChange(e){
-//   setCurrSearch(e.target.value)
-// }
-   const filteredSearch = monster.filter((x) => {
-     return x.name.toLowerCase().includes(currsearch);
-   });
-  
+  const filteredSearch = monster.filter((x) => {
+    return x.name.toLowerCase().includes(currsearch);
+  });
+
   return (
     <div className="App">
-    {currsearch}
-    <br/>
-      rwar
+      <br />
       <input
         type="search"
         placeholder="search for name"
-        // onChange={(e) => handleChange(e)}
-          onChange={(e) =>   {
-            setCurrSearch(e.target.value.toLowerCase())
-         
+        onChange={(e) => {
+          setCurrSearch(e.target.value.toLowerCase());
         }}
       />
       {filteredSearch.map((x) => {
-        // console.log(x.name)
-        return <h1>{x.name}</h1>;
+        return <h1>name: {x.name}</h1>;
       })}
-
     </div>
   );
 }
